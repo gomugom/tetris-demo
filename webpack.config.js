94b5bed7362d7e58cebe6cf9847a0fb5961b3ad3
gomugom <power4ce@gmail.com>
2016-12-04 19:27:02 +0900

    [0] initialize

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var HOST  = 'http://localhost';
+var PORT  = 8080;
+var URL   = HOST + ':' + PORT + '/';
+var _PATH = path.resolve(__dirname, 'src');
+
+module.exports = {
+    HOST: HOST,
+    PORT: PORT,
+    URL: URL,
+    devtool: 'cheap-module-eval-source-map',
+    context: _PATH,
+    entry: {
+        index: [
+            'webpack-dev-server/client?' + HOST + ':' + PORT,
+            'webpack/hot/only-dev-server',
+            path.resolve(_PATH, 'main')
+        ]
+    },
+    output: {
+        path: _PATH,
+        filename: 'bundle.js',
+        publicPath: URL
+    },
+    resolve: {
+        root: [ _PATH ],
+        extensions: [ '', '.js' ]
+    },
+    node: { fs: "empty" },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.EvalSourceMapDevToolPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+};
